fa083f7fc7fdb78b937b163dfce0895e4a93e571
SumuduPerera98 <sharshiperera20@gmail.com>
2022-07-26 15:34:44 +0530

    landing page css

--- Web/gymc-frontend/src/Components/LandingPage/Navbar.jsx
+++ Web/gymc-frontend/src/Components/LandingPage/Navbar.jsx
@@ -19,19 +19,19 @@ export const Navbar = () => {
   return (
     <nav className={nav ? "nav active" :"nav"}>
         <Link to='main' className='logo'>
-            <img src={logo} alt=''/>
+            <img src={logo} smooth={true} duration={1000} alt=''/>
         </Link>
         <input className='menu-btn' type="checkbox" id='menu-btn'/>
         <label className='menu-icon' for='menu-btn'>
             <span className='nav-icon'></span>
         </label>
         <ul className='menu'>
-            <li><Link to='features'>Features</Link></li>
-            <li><Link to='#'>Trainers</Link></li>
-            <li><Link to='pricing'>Pricing</Link></li>
-            <li><Link to='#'>About us</Link></li>
-            <li><Link to='contact'>Contact us</Link></li>
-            <li><Link to='#'>Login</Link></li>
+            <li><Link to='features' smooth={true} duration={1000}>Features</Link></li>
+            <li><Link to='#' smooth={true} duration={1000}>Trainers</Link></li>
+            <li><Link to='pricing' smooth={true} duration={1000}>Pricing</Link></li>
+            <li><Link to='#' smooth={true} duration={1000}>About us</Link></li>
+            <li><Link to='contact' smooth={true} duration={1000}>Contact us</Link></li>
+            <li><Link to='#' smooth={true} duration={1000}>Login</Link></li>
         </ul>
     </nav>
   )
