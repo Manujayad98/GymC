fba8dff9acaedbb57de262df62da4c5f1aac4758
Nilupuli Sanjana <nilupuleethash765@gmail.com>
2022-07-27 14:30:40 +0530

    Contact us sector

--- Web/gymc-frontend/src/App.js
+++ Web/gymc-frontend/src/App.js
@@ -3,15 +3,19 @@ import { Navbar } from './Components/LandingPage/Navbar';
 import { Header } from './Components/LandingPage/Header';
 import Features from './Components/LandingPage/Features';
 import Pricing from './Components/LandingPage/Pricing';
+import About from './Components/LandingPage/About'
+import ContactUs from './Components/LandingPage/ContactUs'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Header/>
-      <Features/>
-      <Pricing/>
+      <Navbar />
+      <Header />
+      <Features />
+      <Pricing />
+      <About />
+      <ContactUs />
     </div>
   );
 }
