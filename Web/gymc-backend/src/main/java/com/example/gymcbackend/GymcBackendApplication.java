9cefea53bb31df0ab4afac87cfd01fe9d9c5eabf
Nilupuli Sanjana <nilupuleethash765@gmail.com>
2022-07-31 23:49:58 +0530

    session handling

--- Web/gymc-backend/src/main/java/com/example/gymcbackend/GymcBackendApplication.java
+++ Web/gymc-backend/src/main/java/com/example/gymcbackend/GymcBackendApplication.java
@@ -1,5 +1,9 @@
 package com.example.gymcbackend;
 
+import com.example.gymcbackend.entities.StaffMember;
+import com.example.gymcbackend.entities.Trainee;
+import com.example.gymcbackend.repository.StaffMemberRepository;
+import com.example.gymcbackend.repository.TraineeRepository;
 import com.example.gymcbackend.repository.UserAccountDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +12,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.annotation.PostConstruct;
 import com.example.gymcbackend.entities.UserAccount;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 @SpringBootApplication
 public class GymcBackendApplication {
@@ -18,6 +25,12 @@ public class GymcBackendApplication {
 	@Autowired
 	private UserAccountDetailsRepository userAccountDetailsRepository;
 
+	@Autowired
+	private StaffMemberRepository staffMemberRepository;
+
+	@Autowired
+	private TraineeRepository traineeRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(GymcBackendApplication.class, args);
 		System.out.println("can login");
@@ -26,18 +39,31 @@ public class GymcBackendApplication {
 	// To init a user
 //	@PostConstruct
 //	protected void init() {
-
+//
 //		UserAccount user=new UserAccount();
+//		StaffMember staffMember= new StaffMember();
+//
+//		staffMember.setFirstName("Sumudu");
+//		staffMember.setLastName("Perera");
+//		staffMember.setAddress("No,487/2, Temple road, Narammala");
+//		staffMember.setGender('F');
+//		staffMember.setNic("199954872540");
+//		staffMember.setPhoneNumber("0714557892");
+//		staffMember.setStaffType(2);
+//
+//		staffMemberRepository.save(staffMember);
+//
+//		int staffMemId= (int) staffMember.getId();
 //
 //		user.setUserName("SumuduP");
-//		user.setMemberId("S000001");
+//		user.setMemberId(staffMemId);
 //		user.setPassword("12345678");
-//
+//		user.setEmail("sumuduperera98@gmail.com");
+//		user.setUserLevel("Owner");
 //		user.setPassword(passwordEncoder.encode("123456789"));
 //		user.setStatus(true);
 //
 //		userAccountDetailsRepository.save(user);
-
 //	}
 
 }
