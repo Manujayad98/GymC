cb8cdf45c82305e5e49b9a1d80d82c2607609dff
Manujayad98 <2019cs027@stu.ucsc.lk>
2022-08-02 18:02:28 +0530

    line erase

--- Web/gymc-frontend/src/App.js
+++ Web/gymc-frontend/src/App.js
@@ -3,34 +3,22 @@ import { Navbar } from './Components/LandingPage/Navbar';
 import { Header } from './Components/LandingPage/Header';
 import Features from './Components/LandingPage/Features';
 import Pricing from './Components/LandingPage/Pricing';
-<<<<<<< HEAD
 import About from './Components/LandingPage/About'
 import ContactUs from './Components/LandingPage/ContactUs'
-=======
 import Contact from './Components/LandingPage/Contact';
 import Trainers from './Components/LandingPage/Trainers';
->>>>>>> Nips
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Navbar />
       <Header />
       <Features />
+      <Trainers/>
       <Pricing />
       <About />
       <ContactUs />
-=======
-      <Navbar/>
-      <Header/>
-      <Features/>
-      <Trainers/>
-      <Pricing/>
-      <Contact/>
-      
->>>>>>> Nips
     </div>
   );
 }
