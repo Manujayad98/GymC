e80f936001c3e1e4d6e5097d8ad0e7bebef1ac57
Nilupuli Sanjana <nilupuleethash765@gmail.com>
2022-08-08 17:50:05 +0530

    file names changed

--- Web/gymc-frontend/src/index.js
+++ Web/gymc-frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { Provider } from 'react-redux';
-import store from '../src/Components/redux/store';
+import store from '../src/Components/Redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
