168d1ba7cad1c28d3037a6a2f91476ab2d92883a
Sudam Munasinghe <85274135+Sudam1998@users.noreply.github.com>
2022-08-15 17:28:54 +0530

    Recept_analytics.jsx

--- Web/gymc-frontend/src/Components/Pages/receptionist/recept_analytics/Analytics.jsx
+++ Web/gymc-frontend/src/Components/Pages/receptionist/recept_analytics/Analytics.jsx
@@ -1,19 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../recept_sidebar/Sidebar.css'
 import SidebarR from '../recept_sidebar/Sidebar'
 import HeaderR from '../recept_header/Header'
+import BarChart from '../../../Utilities/Charts/BarChart'
+
+import './Analytics.css'
 
 const Analytics = () => {
+
+  const [ChartData] = useState([
+    ["Week","Percentage"],
+    ["Week1", 20],
+    ["Week2", 70],
+    ["Week3", 40],
+    ["Week4", 50],
+   
+    
+
+  ]);
+
   return (
     <div className='main-container'>
-      <SidebarR/>
-      <div className='body-container'> 
-          <HeaderR title="Analytics"/>
-          <div className="content-container">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem cum laudantium ipsa laborum esse! Atque temporibus asperiores veritatis ipsum enim ratione quia. Accusantium nesciunt, necessitatibus quaerat ea rerum aliquid minus?</p>
+      <SidebarR />
+      <div className='body-container'>
+        <HeaderR title="Analytics" />
+        <div className="content-container">
+            <div className='recept-analytics-card-container'>
+
+                <div className="cardx">
+                      <div className='recept-analytics-card analytics-cards'>
+                        <div className='recept-analytics-card-content'>
+                          <div className='Name'>Number of Trainers</div>
+                          <div className='Count'>5</div>
+                        </div>
+                      </div>
+                      <div className='recept-analytics-card analytics-cards'>
+                        <div className='recept-analytics-card-content'>
+                          <div className='Name'>Number of Trainees</div>
+                          <div className='Count'>10</div>
+                        </div>
+                  </div>
+              </div>
+              <div className="dropdown">
+                                    
+                                    <select class="form-control form-control-sm" style={{ padding: '8px' }}>
+                                        <option>2018</option>
+                                        <option>2019</option>
+                                        <option>2020</option>
+                                        <option>2021</option>
+                                        <option>2022</option>
+                                    </select>
+                                    
+                                    <select class="form-control form-control-sm" style={{ padding: '8px' }}>
+                                        <option>Jan</option>
+                                        <option>Feb</option>
+                                        <option>Mar</option>
+                                        <option>Apr</option>
+                                        <option>May</option>
+                                        <option>Jun</option>
+                                        <option>Jul</option>
+                                        <option>Aug</option>
+                                        <option>Sep</option>
+                                        <option>Oct</option>
+                                        <option>Nov</option>
+                                        <option>Dec</option>
+                                    </select>
+
+              </div>
           </div>
+
+          
+
+          <div className='recept-analytics-card-container'>
+          
+          </div>
+
+          <div className='recept-analytics-chart-table-container'>
+
+            <div className='recept-analytics-chart-container'>
+              {/* <div className='own-dashboard-container-head'>Annual Income</div> */}
+              <div className='recept-analytics-card chart-cards'>
+                <BarChart data={ChartData} />
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
-    </div>
+    </div >
   )
 }
 
