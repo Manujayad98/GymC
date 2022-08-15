4000b8efe46f545c904d98efa94cb448ea4111a2
SewminiWanigarathna <2019is079@stu.ucsc.cmb.ac.lk>
2022-08-15 15:07:39 +0530

    Dashboard

--- Web/gymc-frontend/src/Components/Pages/receptionist/recept_trainees/Trainees.jsx
+++ Web/gymc-frontend/src/Components/Pages/receptionist/recept_trainees/Trainees.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Deletemodal from '../../../Utilities/Popups/popup';
 import '../recept_sidebar/Sidebar.css'
 import SidebarR from '../recept_sidebar/Sidebar'
 import HeaderR from '../recept_header/Header'
 
+
+
 const Trainees = () => {
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
   return (
     <div className='main-container'>
       <SidebarR/>
       <div className='body-container'> 
           <HeaderR title="Trainees"/>
           <div className="content-container">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem cum laudantium ipsa laborum esse! Atque temporibus asperiores veritatis ipsum enim ratione quia. Accusantium nesciunt, necessitatibus quaerat ea rerum aliquid minus?</p>
+          <Button variant="primary" onClick={handleShow}>
+        Launch static backdrop modal
+      </Button>
+      {show && <Deletemodal show/>}
           </div>
       </div>
     </div>
