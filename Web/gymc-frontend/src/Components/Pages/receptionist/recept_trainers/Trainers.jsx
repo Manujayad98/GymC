09c3c99f09865458f9b5102409fb02b475c1d783
SumuduPerera98 <sharshiperera20@gmail.com>
2022-08-15 11:51:25 +0530

    recept trainer ui

--- Web/gymc-frontend/src/Components/Pages/receptionist/recept_trainers/Trainers.jsx
+++ Web/gymc-frontend/src/Components/Pages/receptionist/recept_trainers/Trainers.jsx
@@ -1,30 +1,113 @@
 import React, { useState } from "react";
 import '../recept_sidebar/Sidebar.css'
-import './Trainers.css'
-import SidebarR from '../recept_sidebar/Sidebar'
-import HeaderR from '../recept_header/Header'
+import SidebarO from '../recept_sidebar/Sidebar'
+import HeaderO from '../recept_header/Header'
 import Table from '../../../Utilities/Tables/Table1'
 import Trash from '../../../../images/Icons/trash-solid.svg'
 import Edit from '../../../../images/Icons/pen-solid.svg'
-import View from '../../../../images/Icons/eye-solid.svg'
+import Arrow from '../../../../images/Icons/arrow-square-right.svg'
 import T1 from '../../../../images/t1.png'
-import Card from '../../../Utilities/Card/Card'
 
+import './Trainers.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 export default function Trainers() {
+
     const [trainerDetails] = useState([
         {
             TrainerImg: (<img src={T1} style={{ borderRadius: "50%" }} height={40} width={40}></img>),
             TrainerID: "T001",
             TrainerName: "RMN Ruwan",
-            Phone: "0714558741",
-            Address: "No. 46/4, Tample road, Dambadeniya",
             RegDate: "2021-10-24",
+            NextShift: "2022-10-17 10.00a.m",
+            Status: (
+                <span >
+                    <button type="button" class="btn" disabled style={{ backgroundColor: '#74E486', width: '90px', padding: '0 5px 0 5px', border: 'none' }}>Leave</button>
+                </span>),
             Actions: (
                 <span >
-                    <span style={{ paddingRight: "20px" }}><img src={View} alt="" height={20} width={20} /></span>
-                    <span style={{ paddingRight: "20px" }}><img src={Edit} alt="" height={20} width={20} /></span>
+                    <span style={{ paddingRight: "20px" }}><img src={Arrow} alt="" height={20} width={20} /></span>
                     <span style={{ paddingRight: "20px" }}><img src={Trash} alt="" height={20} width={20} /></span>
+                    <span style={{ paddingRight: "20px" }}><img src={Edit} alt="" height={20} width={20} /></span>
+                    
+                </span >
+            ),
+        },
+        {
+            TrainerImg: (<img src={T1} style={{ borderRadius: "50%" }} height={40} width={40}></img>),
+            TrainerID: "T002",
+            TrainerName: "KG Hasara",
+            RegDate: "2020-11-11",
+            NextShift: "2022-10-17 10.00a.m",
+            Status: (
+                <span >
+                    <button type="button" class="btn" disabled style={{ backgroundColor: '#74E486', width: '90px', padding: '0 5px 0 5px', border: 'none' }}>Leave</button>
+                </span>),
+            Actions: (
+                <span >
+                    <span style={{ paddingRight: "20px" }}><img src={Arrow} alt="" height={20} width={20} /></span>
+                    <span style={{ paddingRight: "20px" }}><img src={Trash} alt="" height={20} width={20} /></span>
+                    <span style={{ paddingRight: "20px" }}><img src={Edit} alt="" height={20} width={20} /></span>
+
+                </span >
+            ),
+        },
+        {
+            TrainerImg: (<img src={T1} style={{ borderRadius: "50%" }} height={40} width={40}></img>),
+            TrainerID: "T002",
+            TrainerName: "KG Hasara",
+            RegDate: "2020-11-11",
+            NextShift: "2022-10-17 10.00a.m",
+            Status: (
+                <span >
+                    <button type="button" class="btn" disabled style={{ backgroundColor: '#74E486', width: '90px', padding: '0 5px 0 5px', border: 'none' }}>Leave</button>
+                </span>),
+            Actions: (
+                <span >
+                    <span style={{ paddingRight: "20px" }}><img src={Arrow} alt="" height={20} width={20} /></span>
+                    <span style={{ paddingRight: "20px" }}><img src={Trash} alt="" height={20} width={20} /></span>
+                    <span style={{ paddingRight: "20px" }}><img src={Edit} alt="" height={20} width={20} /></span>
+
+                </span >
+            ),
+        },
+        {
+            TrainerImg: (<img src={T1} style={{ borderRadius: "50%" }} height={40} width={40}></img>),
+            TrainerID: "T002",
+            TrainerName: "KG Hasara",
+            RegDate: "2020-11-11",
+            NextShift: "2022-10-17 10.00a.m",
+            Status: (
+                <span >
+                    <button type="button" class="btn" disabled style={{ backgroundColor: '#74E486', width: '90px', padding: '0 5px 0 5px', border: 'none' }}>Leave</button>
+                </span>),
+            Actions: (
+                <span >
+                    <span style={{ paddingRight: "20px" }}><img src={Arrow} alt="" height={20} width={20} /></span>
+                    <span style={{ paddingRight: "20px" }}><img src={Trash} alt="" height={20} width={20} /></span>
+                    <span style={{ paddingRight: "20px" }}><img src={Edit} alt="" height={20} width={20} /></span>
+
+                </span >
+            ),
+        },
+        {
+            TrainerImg: (<img src={T1} style={{ borderRadius: "50%" }} height={40} width={40}></img>),
+            TrainerID: "T002",
+            TrainerName: "KG Hasara",
+            RegDate: "2020-11-11",
+            NextShift: "2022-10-17 10.00a.m",
+            Status: (
+                <span >
+                    <button type="button" class="btn" disabled style={{ backgroundColor: '#74E486', width: '90px', padding: '0 5px 0 5px', border: 'none' }}>Leave</button>
+                </span>),
+            Actions: (
+                <span >
+                    <span style={{ paddingRight: "20px" }}><img src={Arrow} alt="" height={20} width={20} /></span>
+                    <span style={{ paddingRight: "20px" }}><img src={Trash} alt="" height={20} width={20} /></span>
+                    <span style={{ paddingRight: "20px" }}><img src={Edit} alt="" height={20} width={20} /></span>
+
                 </span >
             ),
         },
@@ -35,72 +118,17 @@ export default function Trainers() {
             Phone: "0765584751",
             Address: "No. 46/4, Church road, Giriulla",
             RegDate: "2020-11-11",
+            Status: (
+                <span >
+                    <button type="button" class="btn" disabled style={{ backgroundColor: '#74E486', width: '90px', padding: '0 5px 0 5px', border: 'none' }}>Leave</button>
+                </span>),
             Actions: (
                 <span >
-                    <span style={{ paddingRight: "20px" }}><img src={View} alt="" height={20} width={20} /></span>
-                    <span style={{ paddingRight: "20px" }}><img src={Edit} alt="" height={20} width={20} /></span>
+                    <span style={{ paddingRight: "20px" }}><img src={Arrow} alt="" height={20} width={20} /></span>
                     <span style={{ paddingRight: "20px" }}><img src={Trash} alt="" height={20} width={20} /></span>
+                    <span style={{ paddingRight: "20px" }}><img src={Edit} alt="" height={20} width={20} /></span>
+
                 </span >
-            ),
-        },
-        {
-            TrainerImg: (<img src={T1} style={{ borderRadius: "50%" }} height={40} width={40}></img>),
-            TrainerID: "T002",
-            TrainerName: "KG Hasara",
-            Phone: "0765584751",
-            Address: "No. 46/4, Church road, Giriulla",
-            RegDate: "2020-11-11",
-            Actions: (
-                <span >
-                    <span style={{ paddingRight: "20px" }}><img src={View} alt="" height={20} width={20} /></span>
-                    <span style={{ paddingRight: "20px" }}><img src={Edit} alt="" height={20} width={20} /></span>
-                    <span style={{ paddingRight: "20px" }}><img src={Trash} alt="" height={20} width={20} /></span>
-                </span >
-            ),
-        },
-        {
-            TrainerImg: (<img src={T1} style={{ borderRadius: "50%" }} height={40} width={40}></img>),
-            TrainerID: "T002",
-            TrainerName: "KG Hasara",
-            Phone: "0765584751",
-            Address: "No. 46/4, Church road, Giriulla",
-            RegDate: "2020-11-11",
-            Actions: (
-                <span >
-                    <span style={{ paddingRight: "20px" }}><img src={View} alt="" height={20} width={20} /></span>
-                    <span style={{ paddingRight: "20px" }}><img src={Edit} alt="" height={20} width={20} /></span>
-                    <span style={{ paddingRight: "20px" }}><img src={Trash} alt="" height={20} width={20} /></span>
-                </span >
-            ),
-        },
-        {
-            TrainerImg: (<img src={T1} style={{ borderRadius: "50%" }} height={40} width={40}></img>),
-            TrainerID: "T002",
-            TrainerName: "KG Hasara",
-            Phone: "0765584751",
-            Address: "No. 46/4, Church road, Giriulla",
-            RegDate: "2020-11-11",
-            Actions: (
-                <span >
-                    <span style={{ paddingRight: "20px" }}><img src={View} alt="" height={20} width={20} /></span>
-                    <span style={{ paddingRight: "20px" }}><img src={Edit} alt="" height={20} width={20} /></span>
-                    <span style={{ paddingRight: "20px" }}><img src={Trash} alt="" height={20} width={20} /></span>
-                </span >
-            ),
-        },
-        {
-            TrainerImg: (<img src={T1} style={{ borderRadius: "50%" }} height={40} width={40}></img>),
-            TrainerID: "T002",
-            TrainerName: "KG Hasara",
-            Phone: "0765584751",
-            Address: "No. 46/4, Church road, Giriulla",
-            RegDate: "2020-11-11",
-            Actions: (
-                <span >
-                    <span style={{ paddingRight: "20px" }}><img src={View} alt="" height={20} width={20} /></span>
-                    <span style={{ paddingRight: "20px" }}><img src={Edit} alt="" height={20} width={20} /></span>
-                    <span style={{ paddingRight: "20px" }}><img src={Trash} alt="" height={20} width={20} /></span>
-                </span>
             ),
         },
 
@@ -110,36 +138,50 @@ export default function Trainers() {
         { id: "TrainerImg", label: "", numeric: false },
         { id: "TrainerID", label: "TRAINER ID", numeric: false },
         { id: "TrainerName", label: "TRAINER NAME", numeric: false },
-        { id: "Phone", label: "PHONE", numeric: false },
-        { id: "Address", label: "ADDRESS", numeric: false },
-        { id: "RegDate", label: "REG ON", numeric: false },
+        { id: "RegDate", label: "REG DATE", numeric: false },
+        { id: "NextShift", label: "NEXT SHIFT", numeric: false },
+        { id: "Status", label: "STATUS", numeric: false },
         { id: "Actions", label: "ACTIONS", numeric: false },
     ]);
 
     return (
-        
-        // <div>
-        //     <Table
-        //         rows={trainerDetails}
-        //         headCells={trainerDetailsTableHead}
-        //         tableName={"Trainers"}
-        //     />
-        // </div>
+
         <div className='main-container'>
-      <SidebarR/>
-      <div className='body-container'> 
-          <HeaderR title="Trainers"/>
-          <div className="content-container">
-           
-                <Table
-                    rows={trainerDetails}
-                    headCells={trainerDetailsTableHead}
-                    tableName={"Trainers"} 
-                />
-            
-          </div>
-          
-      </div>
-    </div>
+            <SidebarO />
+            <div className='body-container'>
+                <HeaderO title="Trainers" />
+                <div className="content-container">
+                    <form action="">
+                        <div className="filter-container">
+                            <div className="inputFields-container">
+                                <div className='searchbar-container'>
+                                    <div className="form-label-container">
+                                        <label class="form-label" for="form1">Trainer ID</label>
+                                    </div>
+                                    <div class="input-group">
+                                        <button type="button" class="btn btn-dark" style={{ height: '38px', width: '50px' }}>
+                                            <FontAwesomeIcon icon={faSearch} />
+                                        </button>
+                                        <div class="form-outline">
+                                            <input type="search" id="form1" class="form-control" placeholder='Search here' />
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className="button-container">
+                                <button type="button" class="btn" style={{ backgroundColor: '#3DA2FF', width: '100px' }}>Add</button>
+                            </div>
+
+                        </div>
+                    </form>
+                    <Table
+                        rows={trainerDetails}
+                        headCells={trainerDetailsTableHead}
+                        tableName={"Trainers"}
+                    />
+                </div>
+            </div >
+        </div >
     )
 }
