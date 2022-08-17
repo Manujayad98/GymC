370928d393a35d27601f54461ae6ae03948399f5
SewminiWanigarathna <2019is079@stu.ucsc.cmb.ac.lk>
2022-08-17 13:02:34 +0530

    Receptionist equipments

--- Web/gymc-frontend/src/Components/Pages/receptionist/recept_equipments/Equipments.jsx
+++ Web/gymc-frontend/src/Components/Pages/receptionist/recept_equipments/Equipments.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 import '../recept_sidebar/Sidebar.css'
 import SidebarR from '../recept_sidebar/Sidebar'
 import HeaderR from '../recept_header/Header'
-import Card from '../../../Utilities/Card/Card'
+import { Card } from 'react-bootstrap';
+import Treadmill from '../../../../images/treadmill.png'
+import Dumbbell from '../../../../images/dumbbells.jpg'
+import Bench from '../../../../images/bench.jpg'
+import Plus from '../../../../images/Icons/circle-plus-solid.svg'
+import View from '../../../../images/Icons/eye-solid.svg'
 import './Equipments.css'
 
 
@@ -16,19 +21,42 @@ const Equipments = () => {
           {/* <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem cum laudantium ipsa laborum esse! Atque temporibus asperiores veritatis ipsum enim ratione quia. Accusantium nesciunt, necessitatibus quaerat ea rerum aliquid minus?</p> */}
           
           <div className="content-container">
+            <button type="button" class="btn" style={{ backgroundColor: '#3DA2FF', width: '100px', margin: '20px' }}>Add</button>
             <div className='Ecard'>
-              <Card title="Treadmill" para="10"/>
-              <Card title="Ellipticals" para="8"/>
-              <Card title="Dumbbell sets" para="5"/>
-              <Card title="Treadmill" para="10"/>
-              <Card title="Ellipticals" para="8"/>
-              <Card title="Dumbbell sets" para="5"/>
+              <Card className='rec-equipment-card'>
+                <Card.Title><img src={Treadmill} alt="" height={250} width={250}/></Card.Title>
+                <Card.Subtitle>Treadmills</Card.Subtitle>
+                <Card.Body className='content-row'>
+                <button className='equipment-button'><img src={View} alt="" height={20} width={20} /></button>
+                <button className='equipment-button'><img src={Plus} alt="" height={20} width={20} /></button>
+                </Card.Body>
+              </Card>
+              <Card className='rec-equipment-card'>
+                <Card.Title><img src={Dumbbell} alt="" height={250} width={250}/></Card.Title>
+                <Card.Subtitle>Dumbbells</Card.Subtitle>
+                <Card.Body className='content-row'>
+                <button className='equipment-button'><img src={View} alt="" height={20} width={20} /></button>
+                <button className='equipment-button'><img src={Plus} alt="" height={20} width={20} /></button>
+                </Card.Body>
+              </Card>
+              <Card className='rec-equipment-card'>
+                <Card.Title><img src={Bench} alt="" height={250} width={250}/></Card.Title>
+                <Card.Subtitle>Benches</Card.Subtitle>
+                <Card.Body className='content-row'>
+                <button className='equipment-button'><img src={View} alt="" height={20} width={20} /></button>
+                <button className='equipment-button'><img src={Plus} alt="" height={20} width={20} /></button>
+                </Card.Body>
+              </Card>
+              <Card className='rec-equipment-card'>
+                <Card.Title><img src={Treadmill} alt="" height={250} width={250}/></Card.Title>
+                <Card.Subtitle>Treadmills</Card.Subtitle>
+                <Card.Body className='content-row'>
+                <button className='equipment-button'><img src={View} alt="" height={20} width={20} /></button>
+                <button className='equipment-button'><img src={Plus} alt="" height={20} width={20} /></button>
+                </Card.Body>
+              </Card>
             </div>
           </div>
-            {/* <div>
-              <p>10</p>
-            </div>
-          </Card>  */}
       </div>
       
     </div>
