e2d25164bca9a819755646800061c0fead537f8f
Sudam Munasinghe <85274135+Sudam1998@users.noreply.github.com>
2022-08-24 21:43:39 +0530

    updated view workout

--- Web/gymc-frontend/src/Components/Pages/owner/own_view_workout/ViewWorkout.jsx
+++ Web/gymc-frontend/src/Components/Pages/owner/own_view_workout/ViewWorkout.jsx
@@ -4,14 +4,16 @@ import './ViewWorkout.css'
 import SidebarO from '../own_sidebar/Sidebar'
 import HeaderO from '../own_header/Header'
 import SampleCal from '../../../Utilities/CalendarComp/SampleCal'
-
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 import Pic1 from '../../../../images/owner.png'
-
+import Edit from '../../../../images/Icons/pen-solid.svg'
 // import Button from '../../../Utilities/Form/Button';
 import InputField from "../../../Utilities/Form/InputField";
 import { Validators } from "../../../Utilities/Form/Validator/Validator";
 import Table from '../../../Utilities/Tables/Table2';
 import { Link } from 'react-router-dom'
+
 
 export default function ViewWorkout() {
     const [requestData, setState] = useState({
@@ -48,78 +50,109 @@ export default function ViewWorkout() {
     const handleCheckbox = (acceptance) => {
         this.setState({ acceptance });
     };
-    const [announcementDetailsTableHead] = useState([
-        { id: "Date_Time", label: "Date & Time", numeric: false },
-        { id: "Topic", label: "Topic", numeric: false },
-        { id: "Note", label: "Note", numeric: false },
+    const [workoutDetailsTableHead] = useState([
+        { label:"exercise",id: "incline",numeric: false },
+        { label:"Repitions",id: "reps", numeric: false },
+        { id: "delete", numeric: false },
         
       ]);
-      const [announcementDetails] = useState([
+      const [workoutDetails] = useState([
         {
-          Date_Time: "2022/08/15 09.30 AM",
-          Topic: "Center Closure",
-          Note: "Fitness center is closed on 20 th August 2022",
+          incline: "Incline Press",
+          reps: "15 Reps",
+          delete: <img src={Edit} alt="" height={20} width={20} />,
           
         },
         {
-          Date_Time: "2022/08/15 09.30 AM",
-          Topic: "Center Closure",
-          Note: "Fitness center is closed on 20 th August 2022",
-          
-        },
-        {
-          Date_Time: "2022/08/15 09.30 AM",
-          Topic: "Center Closure",
-          Note: "Fitness center is closed on 20 th August 2022",
-          
-        },
-        {
-          Date_Time: "2022/08/15 09.30 AM",
-          Topic: "Center Closure",
-          Note: "Fitness center is closed on 20 th August 2022"
-           
+            incline: "Incline Press",
+            reps: "15 Reps",
+            delete: <img src={Edit} alt="" height={20} width={20} />,
+            
+          },
+          {
+            incline: "Incline Press",
+            reps: "15 Reps",
+            delete: <img src={Edit} alt="" height={20} width={20} />,
+            
+          },
         
+      
+        
+      ]);
+      const [dietDetailsTableHead] = useState([
+        { label:"Nutrition", id: "nutrition",numeric: false },
+        { label:"Calorie Intake", id: "CalorieIntake", numeric: false },
+        { id: "delete", numeric: false },
+        
+      ]);
+      const [dietDetails] = useState([
+        {
+          nutrition: "Protien",
+          CalorieIntake: "23",
+          delete: <img src={Edit} alt="" height={20} width={20} />,
+          
         },
         {
-          Date_Time: "2022/08/15 09.30 AM",
-          Topic: "Center Closure",
-          Note: "Fitness center is closed on 20 th August 2022",
-        },
+            nutrition: "Carbs",
+            CalorieIntake: "17",
+            delete: <img src={Edit} alt="" height={20} width={20} />,
+            
+          },
+          {
+            nutrition: "Fat",
+            CalorieIntake: "85",
+            delete: <img src={Edit} alt="" height={20} width={20} />,
+            
+          },
+      
         
       ]);
 
-    return (
+      return (
         <div className='main-container'>
-            <SidebarO />
-            <div className='body-container'>
-                <HeaderO title="New Trainer" />
-                <div className="content-container">
-
-                    <div className="workout-container">
-                        <div className="workout-profile-card">profile card
-                            <div className='own-dashboard-card profile-cards'>
-                                <div className='own-dashboard-card-img-container'>
+          <SidebarO />
+          <div className='body-container'>
+            <HeaderO title="Trainees" />
+            <div className="own-trainee-content-container">
+              <div className="left">
+                  <div className="own-trainee-profile">
+                        
+                        <div className="workout-profile-card">
+                            <div className='own-dashboard-card-profile-cards'>
+                                <div className='own-viewworkout-card-img-container'>
                                     <img src={Pic1} alt="" />
                                 </div>
                                 <div className='own-dashboard-card-content'>
-                                    <div className='staffID'>T00001</div>
-                                    <div className='staffName'>Sanjana Rajapaksha</div>
+                                    <div className='tname'>S.Rajapakse</div>
+                                    <div className='tid'>T0001</div>
+                                    
                                 </div>
                             </div>
 
                         </div>
-                        <div className="workout-button-card">buttons here
-                            <button>New Workout</button>
-                            <button>Progress</button>
+                        <div className="workout-button-card">
+                        <Link to=''>
+                            <button class="newworkoutbtn">New Workout</button>
+                        </Link>
+                        <Link to='/Otraineeprogress'>
+                            <button class="progressbtn">Progress</button>
+                        </Link>
+                        
                         </div>
-                        <div className="workout-calendar-card">Calendar
-                            <SampleCal /></div>
-                        <div className="trainee-basic-details-card">
 
-                            <div className='trainee-workout-basic-details-container'>
-                                <div className="login-form-content-left">
-                                    <div className="login-form-in-container">
-                                        <div className="form-inputs">
+
+
+
+
+                  </div>
+                    <div className="own-trainee-form">
+                      
+                      {/* START */}
+
+                      <div className="view_workout_form-inputs">
+                                  <div className="f-content">
+
+
 
                                             <div className="form-row">
                                                 <div className="form-col1">
@@ -137,7 +170,7 @@ export default function ViewWorkout() {
                                             </div>
                                             <div className="form-row">
                                                 <div className="form-col1">
-                                                    <InputField
+                                                    <InputField 
                                                         value={requestData.fullname}
                                                         type='text'
                                                         name='userId'
@@ -149,6 +182,7 @@ export default function ViewWorkout() {
                                                         onChange={handleChange('fullname')} />
                                                 </div>
                                             </div>
+                                         
                                             <div className="form-row">
                                                 <div className="form-col1">
                                                     <InputField
@@ -157,6 +191,7 @@ export default function ViewWorkout() {
                                                         name='userId'
                                                         label="Phone"
                                                         placeholder='Type'
+                                                        readonly
                                                         validators={[
                                                             { check: Validators.required, message: 'This field is required' }
                                                         ]}
@@ -178,40 +213,83 @@ export default function ViewWorkout() {
                                                         onChange={handleChange('password')} />
                                                 </div>
                                             </div>
-
+                                            <div className='form-row'>
+                                                <div className="form-col1">
+                                                    <InputField
+                                                        value={requestData.password}
+                                                        type='text'
+                                                        name='password'
+                                                        label="Address"
+                                                        placeholder='Type'
+                                                        validators={[
+                                                            { check: Validators.required, message: 'This field is required' }
+                                                        ]}
+                                                        onChange={handleChange('password')} />
+                                                </div>
+                                            </div>
 
 
                                         </div>
+
                                     </div>
-                                </div>
 
-                            </div>
+                      {/* END */}
 
 
-                        </div>
-                        <div className="trainee-workout-schedule-view-card">
 
-                            <div className='own-dashboard-table-container'>
-                                <div className='own-dashboard-card table-cards'>
-                                    <Table
-                                        rows={announcementDetails}
-                                        headCells={announcementDetailsTableHead}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="button-container">
-                                <Link to='/OupdateWorkout'>
-                                    <button type="button" class="btn" style={{ backgroundColor: '#3DA2FF', width: '100px' }}>Update Workout</button>
-                                </Link>
-                            </div>
                     </div>
+              </div>
+              <div className="right">
+                  <div className="own-trainee-calender">
+                            <h6 className="calender_title">Select a Date from Calandar</h6>
+                            <div className="own-viewworkout-calender">
+                                    
+                                    <div className="own-viewworkout-calender-card">
+                                        <SampleCal />
+                                    </div>
+                            </div>
+
+                            
+                           
+                  </div>
+                  <div className="own-trainee-table">
+                                <div className="t-content">
+                                <Tabs className="dietworkout_tab">
+                                        <Tab eventKey="home" title="Workout" >
+                                            
+                                            <Table
+                                                rows={workoutDetails}
+                                                headCells={workoutDetailsTableHead}
+                                            />
+
+                                        </Tab>
+                                        <Tab eventKey="profile" title="Diet" >
+                                        <Table
+                                                rows={dietDetails}
+                                                headCells={dietDetailsTableHead}
+                                            />
+
+                                        </Tab>
+
+                                </Tabs>
+                                
+                                  
+                                     
+                                    
+                                </div>
+                                <Link to='/OupdateWorkout'>
+                                    <button className="update_btn">Update</button>
+                                </Link>
 
 
-
-                </div>
-
+                  </div>
+              </div>
+              
             </div>
+    
+          </div>
         </div>
-    )
-}
+      )
+    }
+    
+ 
