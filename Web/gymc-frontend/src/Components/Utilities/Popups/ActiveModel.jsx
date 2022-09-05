4a9ff730c3307bd85da4e3ca9b1348889ffa4fc4
Nilupuli Sanjana <nilupuleethash765@gmail.com>
2022-09-05 13:46:55 +0530

    model changes according to the actions

--- Web/gymc-frontend/src/Components/Utilities/Popups/ActiveModel.jsx
+++ Web/gymc-frontend/src/Components/Utilities/Popups/ActiveModel.jsx
@@ -2,19 +2,18 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import './Modal.css'
 
-const DeleteModal = (props) => {
-    // if (!open) return null
+const ActiveModal = (props) => {
     return (
         <div className="rec-modal-overlay" style={{ zIndex: '999' }}>
             <div className="rec-modal-container">
                 <Button onClick={props.closePopUp} className="rec-modal-closebtn">X</Button>
-                <p className="rec-modal-title">Are you sure you want to<span style={{ color: 'red' }}> delete {props.msg}? </span></p>
-                <div className="delete-model-btn-container">
+                <p className="rec-modal-title">Are you sure you want to <span style={{ color: 'yelow' }}> active {props.msg}? </span></p>
+                <div className="active-model-btn-container">
                     <Button onClick={props.closePopUp} className="rec-modal-btn rec-cancelbtn">Cancel</Button>
-                    <Button onClick={props.handleSubmit} className="rec-modal-btn rec-removebtn">Remove</Button>
+                    <Button onClick={props.handleSubmit} className="rec-modal-btn rec-activebtn">Active</Button>
                 </div>
             </div>
         </div>
     )
 }
-export default DeleteModal
+export default ActiveModal
