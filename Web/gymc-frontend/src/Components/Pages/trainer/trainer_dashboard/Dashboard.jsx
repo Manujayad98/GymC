16d33877b424ae0edf61095bf7d27edae6c11c58
Nilupuli Sanjana <nilupuleethash765@gmail.com>
2022-09-14 23:51:04 +0530

    chartservice, style changes

--- Web/gymc-frontend/src/Components/Pages/trainer/trainer_dashboard/Dashboard.jsx
+++ Web/gymc-frontend/src/Components/Pages/trainer/trainer_dashboard/Dashboard.jsx
@@ -27,64 +27,64 @@ const Dashboard = () => {
   //ANNUAL INCOME CHART
   const Annualdata = [
     ["Element", "Income", { role: "style" }],
-    ["Jan", 8.94, "#b87333"], 
-    ["Feb", 10.49, "silver"], 
+    ["Jan", 8.94, "#b87333"],
+    ["Feb", 10.49, "silver"],
     ["Mar", 19.3, "gold"],
     ["Apr", 21.45, "color: #e5e4e2"],
-    ["May", 18.94, "#b87333"], 
-    ["Jun", 12.49, "silver"], 
+    ["May", 18.94, "#b87333"],
+    ["Jun", 12.49, "silver"],
     ["Jul", 9.3, "gold"],
     ["Aug", 17.45, "color: #e5e4e2"],
-    ["Sep", 18.94, "#b87333"], 
-    ["Oct", 19.49, "silver"], 
+    ["Sep", 18.94, "#b87333"],
+    ["Oct", 19.49, "silver"],
     ["Nov", 15.3, "gold"],
-    ["Dec", 21.45, "color: #e5e4e2"], 
+    ["Dec", 21.45, "color: #e5e4e2"],
   ];
 
   const [appoinmentDetails] = useState([
     {
-      
+
       Time: "8:30A.M",
       TraineeName: "Nilupul Madhuwantha",
-      Type: "Appoinment", 
-      
+      Type: "Appoinment",
+
     },
     {
-      
+
       Time: "10:30A.M",
       TraineeName: "Ishara Rodrigo",
-      Type: "Reservation",   
-      
+      Type: "Reservation",
+
     },
     {
-      
+
       Time: "11:30A.M",
       TraineeName: "Ruwan Gamage",
       Type: "Reservation",
-      
+
     },
     {
-      
+
       Time: "1:00P.M",
       TraineeName: "Imesh Kasthurirathna",
       Type: "Appoinment",
-      
+
     },
     {
-      
+
       Time: "2:00P.M",
       TraineeName: "Mayori Ekanayake",
       Type: "Reservation",
-     
-      
+
+
     },
     {
-      
+
       Time: "3:00P.M",
       TraineeName: "KG Hasara",
       Type: "Appoinment",
-     
-      
+
+
     },
 
   ]);
@@ -96,8 +96,8 @@ const Dashboard = () => {
         <HeaderO title="Dashboard" />
         <div className="content-container">
 
-          
-      
+
+
 
           <div className='trainer-dashboard-chart-table-container'>
 
@@ -113,29 +113,29 @@ const Dashboard = () => {
             <div className='trainer-dashboard-table-container'>
               <div className='trainer-dashboard-container-head'>Appoinments & Reservations</div>
               <div className='trainer-dashboard-card trainer-dashboard-table-cards'>
-              <div className="table-div">
-            <MaterialTable
-              title="Upcoming"
-              columns={[
-                { title: "Time", field: "Time" },
-                { title: "Trainee Name", field: "TraineeName" },
-                { title: "Type", field: "Type" },
-                
+                <div className="table-div">
+                  <MaterialTable
+                    title="Upcoming"
+                    columns={[
+                      { title: "Time", field: "Time" },
+                      { title: "Trainee Name", field: "TraineeName" },
+                      { title: "Type", field: "Type" },
 
-              ]}
-              icons={TableIcons}
-              data={appoinmentDetails}
-         
-              options={{
-                headerStyle: {
-                  backgroundColor: '#1F0106',
-                  color: '#FFF',
-                  hover: '#FFF'
-                }
-              }}
-            />
-            <DeleteModal open={openModal} onClose={() => setOpenModal(false)} />
-          </div>
+
+                    ]}
+                    icons={TableIcons}
+                    data={appoinmentDetails}
+
+                    options={{
+                      headerStyle: {
+                        backgroundColor: '#1F0106',
+                        color: '#FFF',
+                        hover: '#FFF'
+                      }
+                    }}
+                  />
+                  {/* <DeleteModal open={openModal} onClose={() => setOpenModal(false)} /> */}
+                </div>
               </div>
             </div>
           </div>
