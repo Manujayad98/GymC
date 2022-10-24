425d466e62ff01464c9bf825323fe384967f2973
SumuduPerera98 <sharshiperera20@gmail.com>
2022-10-25 00:45:14 +0530

    recept appoinments connect to frontend

--- Web/gymc-frontend/src/Components/Pages/receptionist/recept_appointments/Appointments.jsx
+++ Web/gymc-frontend/src/Components/Pages/receptionist/recept_appointments/Appointments.jsx
@@ -2,22 +2,19 @@ import React, { useState, useEffect } from "react";
 import '../recept_sidebar/Sidebar.css'
 import SidebarO from '../recept_sidebar/Sidebar'
 import HeaderO from '../recept_header/Header'
-import Table from '../../../Utilities/Tables/Table1'
-
+import MaterialTable from "material-table";
+import TableIcons from '../../../Utilities/Tables/ReactTableIcons'
 
 import './Appointments.css'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom'
-import { Button } from "@mui/material";
-
+import { getAppointmentTableDetails} from "../../../../services/AppointmentService";
 
 
 export default function Appointments() {
 
     useEffect(() => {
         checkValidate();
+        getAppointments();
     }, []);
 
     const checkValidate = async () => {
@@ -27,66 +24,17 @@ export default function Appointments() {
         }
     };
 
+    const [appointments, setAppointments] = useState([]);
 
-    const [trainerDetails] = useState([
-        {
-            TraineeID: "T001",
-            TraineeName: "Kasun Perera",
-            TrainerName: "Gihan Sekara",
-            AppDateTime: "2022-10-17 | 09.30a.m",
-            Appduration: "2 hrs"
-        },
-        {
-          TraineeID: "T002",
-          TraineeName: "Rasul Silves",
-          TrainerName: "Piyath Sandaruwan",
-          AppDateTime: "2022-10-17 | 12.30p.m",
-          Appduration: "2 hrs"
-      },
-        {
-          TraineeID: "T006",
-          TraineeName: "Deunwan Wijesekara",
-          TrainerName: "Gihan Sekara",
-          AppDateTime: "2022-10-22 | 09.30a.m",
-          Appduration: "2 hrs"
-      },
-      {
-        TraineeID: "T004",
-        TraineeName: "Natasha Perera",
-        TrainerName: "Piyath Sandaruwan",
-        AppDateTime: "2022-10-17 | 10.30a.m",
-        Appduration: "2 hrs"
-        },
-        {
-          TraineeID: "T005",
-          TraineeName: "Rajeewa Senevirathna",
-          TrainerName: "Kalindu Sampath",
-          AppDateTime: "2022-10-17 | 12.30p.m",
-          Appduration: "2 hrs"
-      },
-      {
-        TraineeID: "T007",
-        TraineeName: "Pasindu Pathberiya",
-        TrainerName: "Piyath Sandaruwan",
-        AppDateTime: "2022-10-17 | 01.00p.m",
-        Appduration: "2 hrs"
-      },
-      {
-        TraineeID: "T003",
-        TraineeName: "Dominic Gape",
-        TrainerName: "Kalindu Sampath",
-        AppDateTime: "2022-10-17 | 02.30p.m",
-        Appduration: "2 hrs"
-      },
-    ]);
-
-    const [trainerDetailsTableHead] = useState([
-        { id: "TraineeID", label: "TRAINEE ID", numeric: false },
-        { id: "TraineeName", label: "TRAINEE NAME", numeric: false },
-        { id: "TrainerName", label: "TRAINER NAME", numeric: false },
-        { id: "AppDateTime", label: "APPOINTMENT DATE", numeric: false },
-        { id: "Appduration", label: "DURATION", numeric: false },
-    ]);
+    const getAppointments = async () => {
+        const res = await getAppointmentTableDetails();
+        console.log(res.data);
+        setAppointments(
+            [...res.data]
+        );
+        console.log(appointments);
+    };
+   
 
     return (
 
@@ -95,33 +43,32 @@ export default function Appointments() {
             <div className='body-container'>
                 <HeaderO title="Appointments" />
                 <div className="content-container">
-                    <form action="">
-                        <div className="filter-container">
-                            <div className="inputFields-container">
-                                <div className='searchbar-container'>
-                                    <div className="form-label-container">
-                                        <label class="form-label" for="form1">Trainer ID</label>
-                                    </div>
-                                    <div class="input-group">
-                                        <button type="button" class="btn btn-dark" style={{ height: '38px', width: '50px' }}>
-                                            <FontAwesomeIcon icon={faSearch} />
-                                        </button>
-                                        <div class="form-outline">
-                                            <input type="search" id="form1" class="form-control" placeholder='Search here' />
-                                        </div>
-                                    </div>
+                <div className="table-div">
+                <MaterialTable
+              title="Appointments"
+              columns={[
+                { title: "Appointment ID", field: "appointmentID" },
+                { title: "Date", field: "date" },
+                { title: "Start Time", field: "start_time" },
+                { title: "End Time", field: "end_time" },
+                { title: "Staff ID", field: "staff_id" },
+                { title: "Trainee ID", field: "trainee_id" },
 
-                                </div>
-                            </div>
+              ]}
+              icons={TableIcons}
+              data={appointments}
+            
 
-                        </div>
-                    </form>
-                    <Table
-                        rows={trainerDetails}
-                        headCells={trainerDetailsTableHead}
-                        tableName={"Trainers"}
-                    />
-                </div>
+              
+              options={{
+                headerStyle: {
+                  backgroundColor: '#1F0106',
+                  color: '#FFF',
+                  hover: '#FFF'
+                }
+              }}
+            />
+                </div></div>
             </div >
         </div >
     )
