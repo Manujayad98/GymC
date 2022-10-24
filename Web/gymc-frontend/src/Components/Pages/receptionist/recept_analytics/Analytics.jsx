7434b596e58410716549a27249ce8d9c25715284
SumuduPerera98 <sharshiperera20@gmail.com>
2022-10-25 01:17:13 +0530

    modifications

--- Web/gymc-frontend/src/Components/Pages/receptionist/recept_analytics/Analytics.jsx
+++ Web/gymc-frontend/src/Components/Pages/receptionist/recept_analytics/Analytics.jsx
@@ -78,43 +78,13 @@ const [totalOnlinePayments,setTotalOnlinePayments] = useState([]);
       <div className='body-container'>
         <HeaderR title="Transactions" />
         <div className="content-container">
-          {/* <Tabs 
-            defaultActiveKey="Cash"
-            className='mb-3'
-          >
-            <Tab eventKey="Cash" title="Cash Payments">
-              <div className='rec-analytics-card-container'>
-                <div className='rec-analytics-total-card'>
-                  <div className='rec-analytics-card-title'>Total Today</div>
-                  <div className='rec-analytics-card-value'>Rs. 1500</div>
-                </div>
-              </div>
-            </Tab>
-            <Tab eventKey="Other" title="Other Payments">
-            `<div className='rec-analytics-card-container'>
-                <div className='rec-analytics-total-card'>
-                  <div className='rec-analytics-card-title'>Total Today</div>
-                  <div className='rec-analytics-card-value'>Rs. 1500</div>
-                </div>
-              </div>
-            </Tab>
-          </Tabs> */}
+          
           <div className='rep-transactions-cards-container'>
-          {/* {Object.values(totalPayments).map((total) => (
-              <div className='rec-analytics-card-container'>
-                
-                <div className='rec-analytics-total-card'>
-                <div className='rec-analytics-card-title'>Total Payments</div>
-                  <div className='rec-analytics-card-value'>{totalPayments}</div>
-                  <div className='staffName'>{trainer.full_name}</div>
-                </div>
-              </div>
-            ))} */}
+          
           <div className='rec-analytics-card-container'>
               <div className='rec-analytics-total-card'>
                 <div className='rec-analytics-card-title'>Total Payments</div>
                 <div className='rec-analytics-card-value'>Rs. {totalPayments}</div>
-                {/* <div className='rec-analytics-card-value'>abc</div> */}
                 
               </div>
           </div>
@@ -145,38 +115,7 @@ const [totalOnlinePayments,setTotalOnlinePayments] = useState([]);
               ]}
               icons={TableIcons}
               data={transactions}
-              // actions={[
-              //   {
-              //     icon: () => {
-              //       return (
-
-              //         <span style={{ paddingRight: "20px", cursor: 'pointer' }}><img src={Arrow} alt="" height={20} width={20} /></span>
-              //       );
-              //     },
-              //     onClick: (event, rowData) => {
-
-              //     },
-              //   },
-
-              //   {
-              //       icon: () => {
-              //           return (
-
-              //               <button
-              //                   type="button"
-              //                   className="btn mt-0"
-              //                   style={{
-              //                       backgroundColor: "#FF2121",
-              //                       border: "none",
-              //                   }}
-              //               >
-              //                   Leave
-              //               </button>
-              //           );
-              //       }}
-                
-
-              // ]}
+            
 
               
               options={{
