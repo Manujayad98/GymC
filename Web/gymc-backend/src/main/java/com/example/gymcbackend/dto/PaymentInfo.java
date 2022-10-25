fca185f78c43cad51f9f36becd11d13c0612772d
SewminiWanigarathna <2019is079@stu.ucsc.cmb.ac.lk>
2022-10-25 19:38:23 +0530

    Changes

--- Web/gymc-backend/src/main/java/com/example/gymcbackend/dto/PaymentInfo.java
+++ Web/gymc-backend/src/main/java/com/example/gymcbackend/dto/PaymentInfo.java
@@ -1,32 +1,32 @@
-package com.example.gymcbackend.dto;
-
-import java.util.Date;
-
-public class PaymentInfo {
-    private String amount;
-    private String type;
-
-    private String paymentPlanMethodsID;
-    private String trainee_id;
-    private Date date;
-
-
-    public String getAmount() {
-        return amount;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public String getTrainee_id() {
-        return trainee_id;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-
-
-}
+//package com.example.gymcbackend.dto;
+//
+//import java.util.Date;
+//
+//public class PaymentInfo {
+//    private String amount;
+//    private String type;
+//
+//    private String paymentPlanMethodsID;
+//    private String trainee_id;
+//    private Date date;
+//
+//
+//    public String getAmount() {
+//        return amount;
+//    }
+//
+//    public String getType() {
+//        return type;
+//    }
+//
+//    public String getTrainee_id() {
+//        return trainee_id;
+//    }
+//
+//    public Date getDate() {
+//        return date;
+//    }
+//
+//
+//
+//}
--- Web/gymc-backend/src/main/java/com/example/gymcbackend/services/CashPaymentService.java
+++ Web/gymc-backend/src/main/java/com/example/gymcbackend/services/CashPaymentService.java
@@ -1,51 +1,51 @@
-package com.example.gymcbackend.services;
-
-import com.example.gymcbackend.dto.ExerciseTableData;
-import com.example.gymcbackend.dto.PaymentInfo;
-import com.example.gymcbackend.dto.TraineeInfo;
-import com.example.gymcbackend.entities.Payment;
-import com.example.gymcbackend.repository.cashPaymentDao.CashPaymentJdbcRepository;
-import com.example.gymcbackend.repository.cashPaymentDao.CashPaymentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class CashPaymentService {
-    @Autowired
-    CashPaymentJdbcRepository cashPaymentJdbcRepository;
-
-    @Autowired
-    CashPaymentRepository cashPaymentRepository;
-
-    public String cashPayment(Payment payment){
-
-        Payment newPayment = new Payment();
-
-        newPayment.setPaymentPlan(payment.getPaymentPlan());
-        newPayment.setDate(payment.getDate());
-        newPayment.setAmount(payment.getAmount());
-        newPayment.setTrainee(payment.getTrainee());
-        newPayment.setType(payment.getType());
-
-        cashPaymentRepository.save(newPayment);
-        return "payment Added";
-    }
-
-    public PaymentInfo getAllPayments(long trainee_ID) {
-
-        System.out.println("awaservice");
-        //does this work?
-        PaymentInfo payment = cashPaymentJdbcRepository.getPaymentById(trainee_ID);
-
-        System.out.println(trainee_ID);
-        return cashPaymentJdbcRepository.getPaymentById(trainee_ID);
-
-    }
-
-
-    //public List<PaymentInfo> getPlanInfo(){return cashPaymentJdbcRepository.getPlanInfo();}
-
-
-}
+//package com.example.gymcbackend.services;
+//
+//import com.example.gymcbackend.dto.ExerciseTableData;
+//import com.example.gymcbackend.dto.PaymentInfo;
+//import com.example.gymcbackend.dto.TraineeInfo;
+//import com.example.gymcbackend.entities.Payment;
+//import com.example.gymcbackend.repository.cashPaymentDao.CashPaymentJdbcRepository;
+//import com.example.gymcbackend.repository.cashPaymentDao.CashPaymentRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class CashPaymentService {
+//    @Autowired
+//    CashPaymentJdbcRepository cashPaymentJdbcRepository;
+//
+//    @Autowired
+//    CashPaymentRepository cashPaymentRepository;
+//
+//    public String cashPayment(Payment paymentPost){
+//
+//        Payment newPayment = new Payment();
+//
+//        newPayment.setPaymentPlan(paymentPost.getPaymentPlan());
+//        newPayment.setDate(paymentPost.getDate());
+//        newPayment.setAmount(paymentPost.getAmount());
+//        newPayment.setTrainee(paymentPost.getTrainee());
+//        newPayment.setType(paymentPost.getType());
+//
+//        cashPaymentRepository.save(newPayment);
+//        return "payment Added";
+//    }
+//
+//    public PaymentInfo getAllPayments(long trainee_ID) {
+//
+//        System.out.println("awaservice");
+//        //does this work?
+//        PaymentInfo payment = cashPaymentJdbcRepository.getPaymentById(trainee_ID);
+//
+//        System.out.println(trainee_ID);
+//        return cashPaymentJdbcRepository.getPaymentById(trainee_ID);
+//
+//    }
+//
+//
+//    //public List<PaymentInfo> getPlanInfo(){return cashPaymentJdbcRepository.getPlanInfo();}
+//
+//
+//}
