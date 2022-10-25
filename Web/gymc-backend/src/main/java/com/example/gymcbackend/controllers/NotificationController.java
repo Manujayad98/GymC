3286ea5bfec6c52e8ea93ee61bba7319e3061618
Manujayad98 <2019cs027@stu.ucsc.lk>
2022-10-25 20:07:04 +0530

    notification mobile

--- Web/gymc-backend/src/main/java/com/example/gymcbackend/controllers/NotificationController.java
+++ Web/gymc-backend/src/main/java/com/example/gymcbackend/controllers/NotificationController.java
@@ -1,20 +1,31 @@
 package com.example.gymcbackend.controllers;
 
 import com.example.gymcbackend.dto.NotificationResponse;
+import com.example.gymcbackend.entities.TimeSlot;
 import com.example.gymcbackend.services.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:3000")
 public class NotificationController {
 
     @Autowired
     NotificationService notificationService;
 
-    @GetMapping("/unreadNotifications")
-    public List<NotificationResponse> getUnreadNotifications(){
-        return notificationService.getUnreadNotify();
+
+    @GetMapping("/unreadNotifications/{userId}")
+    public List<NotificationResponse> getUnreadNotifications(@PathVariable long userId){
+        return notificationService.getUnreadNotify(userId);
+    }
+
+    @GetMapping("/readNotifications/{userId}")
+    public List<NotificationResponse> getReadNotifications(@PathVariable long userId){
+        return notificationService.getReadNotify(userId);
     }
 
 }
