7683e5a38a0c1e13bdd9ee61121ea07400565a58
SumuduPerera98 <sharshiperera20@gmail.com>
2022-10-27 09:28:12 +0530

    profile chnges

--- Web/gymc-backend/src/main/java/com/example/gymcbackend/controllers/UserController.java
+++ Web/gymc-backend/src/main/java/com/example/gymcbackend/controllers/UserController.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class UserController {
     @Autowired
     private UserAccountDetailsRepository userAccountDetailsRepository;
